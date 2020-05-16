1ddd7df958cfeea7a564a81434e0b1dc0a90e600
hzhao1 <hzhao1@xiaohongshu.com>
2020-05-16 12:39:57 +0800

    fix

---
+++ deploy/index.js
@@ -0,0 +1,25 @@
+const scpClient = require('scp2')
+const ora = require('ora')
+const chalk = require('chalk')
+const server = require('./products')
+const spinner = ora('正在发布到' + (process.env.NODE_ENV === 'prod' ? '生产' : '测试') + '服务器...')
+spinner.start()
+scpClient.scp(
+  'dist/',
+  {
+    host: server.host,
+    port: server.port,
+    username: server.username,
+    password: server.password,
+    path: server.path
+  },
+  function (err) {
+    spinner.stop()
+    if (err) {
+      console.log(chalk.red('发布失败.\n'))
+      throw err
+    } else {
+      console.log(chalk.green('Success! 成功发布到' + (process.env.NODE_ENV === 'prod' ? '生产' : '测试') + '服务器! \n'))
+    }
+  }
+)
