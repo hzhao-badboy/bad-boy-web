e988e544e96044c155bf809f696dd381558e6c65
hzhao1 <hzhao1@xiaohongshu.com>
2020-05-20 15:50:43 +0800

    fix webpack conf

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   publicPath: './',
@@ -17,6 +18,29 @@ module.exports = {
           // 当然如果你的接口地址上本来就有@url，解析之后认识争取的
         }
       }
+    }
+  },
+  chainWebpack: config => {
+    // 清除css，js版本号
+    config.output.filename('js/[name].js').end();
+    config.output.chunkFilename('js/[name].js').end();
+    // 为生产环境修改配置...
+    config.plugin('extract-css').tap(args => [{
+      filename: `css/[name].css`,
+      chunkFilename: `css/[name].css`
+    }]);
+  },
+  // gzip 配置
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 生产环境
+      return {
+        plugins: [new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css/, // 匹配文件名
+          threshold: 1024, // 文件压缩阈值，对超过10k的进行压缩
+          deleteOriginalAssets: false // 是否删除源文件
+        })]
+      };
     }
   }
 };
